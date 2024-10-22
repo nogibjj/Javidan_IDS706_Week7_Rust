cdc8348b5875afec62dd2d4fea5527fcfe2f90bc
Javidan Karimli <javidan@javidans-mbp.lan>
2024-10-22 19:55:58 -0400

    Addition of CLI

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
-// main.rs
-fn main() {
-    let token1 = "Data Engineer";
-    let token2 = "Data Scientist";
+use clap::{Parser, Subcommand};
 
-    let res = main::levenshtein_distance_dp(token1, token2);
-    println!("Levenshtein Distance: {}", res);
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    Calculate {
+        /// The first string
+        token1: String,
+        /// The second string
+        token2: String,
+    },
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    match args.command {
+        Commands::Calculate { token1, token2 } => {
+            let res = main::levenshtein_distance_dp(&token1, &token2);
+            println!("Levenshtein Distance between '{}' and '{}' is: {}", token1, token2, res);
+        }
+    }
 }
